d5a902fb2cb0d007ebd546d798e822b3574e6187
AdrianDH7 <adriandiazhurtado9@gmail.com>
2025-11-29 02:26:12 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,11 @@
-// CONFIGURACIÓN GLOBAL DEL BOT ADRIBOT
 export default {
-    owners: ["5210000000000"], // ← TU NÚMERO AQUÍ
-    prefix: ".",               // prefijo modificable
+    owners: ["5210000000000"], // CAMBIA ESTO
+    prefix: ".",
     botName: "ADRIBOT",
 
     messages: {
         admin: "❌ Este comando es solo para *ADMINISTRADORES*.",
-        owner: "❌ Este comando es solo para *MI CREADOR*.",
         group: "❌ Este comando solo funciona en *GRUPOS*.",
-        private: "❌ Este comando solo funciona en *PRIVADO*.",
-        botAdmin: "⚠️ Necesito ser *ADMIN* para usar este comando.",
         wait: "⏳ Procesando...",
         done: "✔️ Listo!"
     }
