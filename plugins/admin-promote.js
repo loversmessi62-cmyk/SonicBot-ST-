c705268db957f187b745375e35ca35eb8270e0de
AdrianDH7 <adriandiazhurtado9@gmail.com>
2025-11-29 14:34:01 -0600

    Create admin-promote.js

---
+++ plugins/admin-promote.js
@@ -0,0 +1,19 @@
+export default {
+    commands: ["promote", "admin"],
+    category: "administracion",
+    admin: true,
+    description: "Da admin al usuario respondido.",
+
+    async run(sock, msg) {
+        const jid = msg.key.remoteJid;
+
+        const target =
+            msg.message?.extendedTextMessage?.contextInfo?.participant;
+
+        if (!target)
+            return sock.sendMessage(jid, { text: "❌ Responde a alguien para promoverlo." }, { quoted: msg });
+
+        await sock.groupParticipantsUpdate(jid, [target], "promote");
+        await sock.sendMessage(jid, { text: `👑 Ahora es administrador.` }, { quoted: msg });
+    }
+};
