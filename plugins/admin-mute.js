bc33a324b7eda3120421827ffb1a4b05120dd83d
AdrianDH7 <adriandiazhurtado9@gmail.com>
2025-11-29 14:34:56 -0600

    Create admin-mute.js

---
+++ plugins/admin-mute.js
@@ -0,0 +1,31 @@
+import fs from "fs";
+
+export default {
+    commands: ["mute"],
+    category: "administracion",
+    admin: true,
+    description: "Borra todos los mensajes del usuario muteado.",
+
+    async run(sock, msg) {
+        const jid = msg.key.remoteJid;
+        const file = "./mutelist.json";
+
+        const target =
+            msg.message?.extendedTextMessage?.contextInfo?.participant;
+
+        if (!target)
+            return sock.sendMessage(jid, { text: "❌ Responde a quien quieres mutear." }, { quoted: msg });
+
+        let data = [];
+        if (fs.existsSync(file)) data = JSON.parse(fs.readFileSync(file));
+
+        if (!data.includes(target)) data.push(target);
+
+        fs.writeFileSync(file, JSON.stringify(data));
+
+        await sock.sendMessage(jid, {
+            text: `🔇 Usuario muteado:\n@${target.split("@")[0]}`,
+            mentions: [target]
+        });
+    }
+};
