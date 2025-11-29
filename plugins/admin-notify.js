861c59c657e8cb974a2f73661b2d591c86d19530
AdrianDH7 <adriandiazhurtado9@gmail.com>
2025-11-29 13:44:37 -0600

    Update admin-notify.js

--- plugins/admin-notify.js
+++ plugins/admin-notify.js
@@ -1,21 +1,14 @@
 export default {
     commands: ["n"],
-    admin: true, // 🔥 Indica al handler que es solo para admins
+    admin: true,
 
     async run(sock, msg, args, ctx) {
         const jid = msg.key.remoteJid;
 
-        if (!ctx.isAdmin)
-            return sock.sendMessage(jid, { text: "❌ *Solo los admins pueden usar este comando.*" });
-
         const texto = args.join(" ") || "Aviso importante";
 
-        await sock.sendMessage(
-            jid,
-            {
-                text: `📢 *AVISO DEL ADMIN*\n\n${texto}`
-            },
-            { quoted: msg }
-        );
+        await sock.sendMessage(jid, {
+            text: `📢 *AVISO DEL ADMIN*\n\n${texto}`
+        }, { quoted: msg });
     }
 };
