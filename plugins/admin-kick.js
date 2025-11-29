7387f0979260a438b8b22a18771cde5eba59f3b4
AdrianDH7 <adriandiazhurtado9@gmail.com>
2025-11-29 14:33:45 -0600

    Create admin-kick.js

---
+++ plugins/admin-kick.js
@@ -0,0 +1,19 @@
+export default {
+    commands: ["kick", "ban"],
+    category: "administracion",
+    admin: true,
+    description: "Expulsa al usuario respondido.",
+
+    async run(sock, msg) {
+        const jid = msg.key.remoteJid;
+
+        const target =
+            msg.message?.extendedTextMessage?.contextInfo?.participant;
+
+        if (!target)
+            return sock.sendMessage(jid, { text: "❌ Responde al usuario que quieres expulsar." }, { quoted: msg });
+
+        await sock.groupParticipantsUpdate(jid, [target], "remove");
+        await sock.sendMessage(jid, { text: `🦶 Usuario expulsado.` }, { quoted: msg });
+    }
+};
