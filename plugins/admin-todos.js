648dd72f4704e47acbc213b29a899e3c984f3cd2
AdrianDH7 <adriandiazhurtado9@gmail.com>
2025-11-29 13:33:24 -0600

    Update admin-todos.js

--- plugins/admin-todos.js
+++ plugins/admin-todos.js
@@ -1,31 +1,14 @@
-import config from "../config.js";
-
 export default {
-    commands: ["todos", "all", "tagall"],
+    commands: ["todos"],
+    adminOnly: true,
 
-    run: async (sock, msg, args, ctx) => {
-        const { isGroup, isAdmin, groupMetadata } = ctx;
+    async run(sock, msg) {
+
         const jid = msg.key.remoteJid;
 
-        if (!isGroup)
-            return sock.sendMessage(jid, { text: config.messages.group });
-
-        if (!isAdmin)
-            return sock.sendMessage(jid, { text: config.messages.admin });
-
-        const texto = args.join(" ") || "📢 *Atención a todos!*";
-
-        const menciones = groupMetadata.participants.map(p => p.id);
-
-        let mensaje = `👥 *MENCIÓN MASIVA:*\n${texto}\n\n`;
-
-        for (let user of groupMetadata.participants) {
-            mensaje += `• @${user.id.split("@")[0]}\n`;
-        }
-
         await sock.sendMessage(jid, {
-            text: mensaje,
-            mentions: menciones
-        });
+            text: "🔊 @todos",
+            mentions: []
+        }, { quoted: msg });
     }
 };
