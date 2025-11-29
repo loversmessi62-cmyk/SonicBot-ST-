22f9a59a3682826b0c245af5a55f9a114b98f657
AdrianDH7 <adriandiazhurtado9@gmail.com>
2025-11-29 14:35:54 -0600

    Create admin-cerrar grupo.js

---
+++ plugins/admin-cerrar grupo.js
@@ -0,0 +1,13 @@
+export default {
+    commands: ["cerrar"],
+    category: "administracion",
+    admin: true,
+    description: "Cierra el grupo para que solo administradores escriban.",
+
+    async run(sock, msg) {
+        const jid = msg.key.remoteJid;
+
+        await sock.groupSettingUpdate(jid, "announcement");
+        await sock.sendMessage(jid, { text: "🔒 *Grupo cerrado.*" }, { quoted: msg });
+    }
+};
