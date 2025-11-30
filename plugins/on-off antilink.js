b370496a2301063c2c4592447567efa9352d2280
AdrianDH7 <adriandiazhurtado9@gmail.com>
2025-11-29 23:37:40 -0600

    Update and rename admin-antilink.js to on-off antilink.js

---
+++ plugins/on-off antilink.js
@@ -0,0 +1,26 @@
+import { getState, toggleState } from "../utils/cmdToggle.js";
+
+export default {
+    commands: ["antilink"],
+    admin: true,
+    category: "on/off",
+
+    async run(sock, msg, args, ctx) {
+        const jid = msg.key.remoteJid;
+
+        if (!ctx.isGroup)
+            return sock.sendMessage(jid, { text: "❌ Este comando solo funciona en grupos." });
+
+        // Estado actual
+        const status = getState("antilink");
+
+        // Cambiar estado
+        toggleState("antilink");
+
+        const newStatus = getState("antilink");
+
+        await sock.sendMessage(jid, {
+            text: `🛡️ *Antilink ahora está:* ${newStatus ? "🟢 ACTIVADO" : "🔴 DESACTIVADO"}`
+        });
+    }
+};
