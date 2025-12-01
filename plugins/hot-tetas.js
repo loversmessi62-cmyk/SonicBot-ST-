d36652e87e75c302e69120924cb4aa52bef8ca31
AdrianDH7 <adriandiazhurtado9@gmail.com>
2025-11-30 23:49:32 -0600

    Update hot-tetas.js

--- plugins/hot-tetas.js
+++ plugins/hot-tetas.js
@@ -6,7 +6,7 @@ export default {
         const jid = ctx.jid;
 
         const links = [
-            "https://yourlink1.jpg",
+            "https://files.catbox.moe/v7vsn8.bin",
             "https://yourlink2.png",
             "https://yourlink3.mp4"
         ];
