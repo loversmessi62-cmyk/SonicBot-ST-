4cea62354f92ef827f8b49bca42fb115ff6821f5
AdrianDH7 <adriandiazhurtado9@gmail.com>
2025-11-30 23:02:06 -0600

    Create hot-tetas.js

---
+++ plugins/hot-tetas.js
@@ -0,0 +1,25 @@
+export default {
+    commands: ["tetas"],
+    category: "hot",
+
+    async run(sock, msg, args, ctx) {
+        const jid = ctx.jid;
+
+        const links = [
+            "https://yourlink1.jpg",
+            "https://yourlink2.png",
+            "https://yourlink3.mp4"
+        ];
+
+        if (links.length === 0)
+            return sock.sendMessage(jid, { text: "⚠ No hay links en el comando .boobs" });
+
+        const random = links[Math.floor(Math.random() * links.length)];
+        const isVideo = random.endsWith(".mp4") || random.endsWith(".mov");
+
+        await sock.sendMessage(jid, {
+            [isVideo ? "video" : "image"]: { url: random },
+            caption: "🍒 *BOOBS*"
+        });
+    }
+};
