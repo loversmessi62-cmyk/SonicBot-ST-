76603f81915d2dd9e61a95f6994fe391ea22283a
AdrianDH7 <adriandiazhurtado9@gmail.com>
2025-12-16 15:49:14 -0600

    Update admin-unmute.js

--- plugins/admin-unmute.js
+++ plugins/admin-unmute.js
@@ -1,27 +1,22 @@
-import fs from "fs";
+import { unmuteUser } from "../utils/muteState.js";
 
 export default {
     commands: ["unmute"],
-    category: "admin",
     admin: true,
-    description: "Le permite escribir a un usuario nuevamente.",
+    category: "admin",
 
     async run(sock, msg) {
         const jid = msg.key.remoteJid;
-        const file = "./mutelist.json";
 
         const target =
             msg.message?.extendedTextMessage?.contextInfo?.participant;
 
         if (!target)
-            return sock.sendMessage(jid, { text: "❌ Responde al usuario que quieres desmutear." }, { quoted: msg });
+            return sock.sendMessage(jid, {
+                text: "❌ Responde al mensaje del usuario que quieres desmutear."
+            }, { quoted: msg });
 
-        let data = [];
-        if (fs.existsSync(file)) data = JSON.parse(fs.readFileSync(file));
-
-        data = data.filter(u => u !== target);
-
-        fs.writeFileSync(file, JSON.stringify(data));
+        unmuteUser(jid, target);
 
         await sock.sendMessage(jid, {
             text: `🔊 Usuario desmuteado:\n@${target.split("@")[0]}`,
