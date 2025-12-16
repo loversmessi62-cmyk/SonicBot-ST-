0d9e837a971862b7ca2f15f0b963a999b47b67f2
AdrianDH7 <adriandiazhurtado9@gmail.com>
2025-12-16 15:49:04 -0600

    Update admin-mute.js

--- plugins/admin-mute.js
+++ plugins/admin-mute.js
@@ -1,27 +1,22 @@
-import fs from "fs";
+import { muteUser } from "../utils/muteState.js";
 
 export default {
     commands: ["mute"],
-    category: "admin",
     admin: true,
-    description: "Borra todos los mensajes del usuario muteado.",
+    category: "admin",
 
     async run(sock, msg) {
         const jid = msg.key.remoteJid;
-        const file = "./mutelist.json";
 
         const target =
             msg.message?.extendedTextMessage?.contextInfo?.participant;
 
         if (!target)
-            return sock.sendMessage(jid, { text: "❌ Responde a quien quieres mutear." }, { quoted: msg });
+            return sock.sendMessage(jid, {
+                text: "❌ Responde al mensaje del usuario que quieres mutear."
+            }, { quoted: msg });
 
-        let data = [];
-        if (fs.existsSync(file)) data = JSON.parse(fs.readFileSync(file));
-
-        if (!data.includes(target)) data.push(target);
-
-        fs.writeFileSync(file, JSON.stringify(data));
+        muteUser(jid, target);
 
         await sock.sendMessage(jid, {
             text: `🔇 Usuario muteado:\n@${target.split("@")[0]}`,
