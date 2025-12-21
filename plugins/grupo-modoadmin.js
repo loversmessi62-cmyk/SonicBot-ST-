bf404459ba0044df0f375cd45a318fa4a3c278cf
AdrianDH7 <adriandiazhurtado9@gmail.com>
2025-12-21 10:07:50 -0600

    Crear grupo-modoadmin.js

---
+++ plugins/grupo-modoadmin.js
@@ -0,0 +1,33 @@
+import { isModoAdminsEnabled, setModoAdmins } from "../utils/modoadminsState.js";
+
+export default {
+  commands: ["modoadmins"],
+  admin: true,
+  category: "admin",
+
+  async run(sock, msg, args, ctx) {
+    const { jid } = ctx;
+
+    const option = args[0]?.toLowerCase();
+
+    if (!option || !["on", "off"].includes(option)) {
+      return sock.sendMessage(jid, {
+        text: `⚙️ Uso correcto:\n\n.modoadmins on\n.modoadmins off`
+      });
+    }
+
+    if (option === "on") {
+      setModoAdmins(true);
+      return sock.sendMessage(jid, {
+        text: "🔒 *Modo Admin ACTIVADO*\nSolo admins pueden usar el bot."
+      });
+    }
+
+    if (option === "off") {
+      setModoAdmins(false);
+      return sock.sendMessage(jid, {
+        text: "🔓 *Modo Admin DESACTIVADO*\nTodos pueden usar el bot."
+      });
+    }
+  }
+};
