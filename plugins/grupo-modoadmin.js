8e3c09e50867258fc4bac9d70de1b2dd6c998915
AdrianDH7 <adriandiazhurtado9@gmail.com>
2025-12-21 10:32:21 -0600

    Actualizar grupo-modoadmin.js

--- plugins/grupo-modoadmin.js
+++ plugins/grupo-modoadmin.js
@@ -2,31 +2,34 @@ import { isModoAdminsEnabled, setModoAdmins } from "../lib/modoadminsState.js";
 
 export default {
   commands: ["modoadmins"],
-  admin: true,
   category: "admin",
+  admin: true,
 
   async run(sock, msg, args, ctx) {
-    const { jid } = ctx;
+    const { jid, isAdmin } = ctx;
+
+    // 🛑 ESTA es la clave
+    if (!isAdmin) return;
 
     const option = args[0]?.toLowerCase();
 
     if (!option || !["on", "off"].includes(option)) {
       return sock.sendMessage(jid, {
-        text: `⚙️ Uso correcto:\n\n.modoadmins on\n.modoadmins off`
+        text: "⚙️ Uso:\n.modoadmins on\n.modoadmins off"
       });
     }
 
     if (option === "on") {
       setModoAdmins(true);
       return sock.sendMessage(jid, {
-        text: "🔒 *Modo Admin ACTIVADO*\nSolo admins pueden usar el bot."
+        text: "🔒 *Modo Admin ACTIVADO*"
       });
     }
 
     if (option === "off") {
       setModoAdmins(false);
       return sock.sendMessage(jid, {
-        text: "🔓 *Modo Admin DESACTIVADO*\nTodos pueden usar el bot."
+        text: "🔓 *Modo Admin DESACTIVADO*"
       });
     }
   }
