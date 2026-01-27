484cad396182a1f10c60035a8e13d95bcbf77d69
AdrianDH7 <adriandiazhurtado9@gmail.com>
2026-01-27 01:38:13 -0600

    Crear funny-amor.js

---
+++ plugins/funny-amor.js
@@ -0,0 +1,21 @@
+export default {
+  commands: ["amor"],
+  category: "funny",
+
+  async run(sock, msg, args, ctx) {
+    const target =
+      msg.mentionedJid?.[0] ||
+      msg.quoted?.sender
+
+    if (!target)
+      return sock.sendMessage(ctx.jid, { text: "❤️ Menciona o responde a alguien" })
+
+    const percent = Math.floor(Math.random() * 101)
+    const name = target.split("@")[0]
+
+    await sock.sendMessage(ctx.jid, {
+      text: `💘 *@${name}* tiene **${percent}%** de amor hoy`,
+      mentions: [target]
+    })
+  }
+}
