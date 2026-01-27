68c660b463dffec676f6c0f1550bcd57eaceff1a
loversmessi62-cmyk <loversmessi62@gmail.com>
2026-01-27 13:59:19 -0600

    Crear comprarbot.js

---
+++ plugins/comprarbot.js
@@ -0,0 +1,17 @@
+const handler = async (m, { conn }) => {
+  m.reply(global.ComprarBot);
+};
+
+handler.command = /^(comprarbot|comprar)$/i;
+export default handler;
+
+global.ComprarBot = `
+🔹 VENTA DE BOTS 🔹
+
+Automatiza tu grupo y recibe soporte 24/7
+
+BOT PARA GRUPO: 📲 wa.me/+522731590195
+BOT PARA GRUPO PERMANENTE: 📲 wa.me/+522731307252
+
+⚡ Rápido • Seguro • Personalizado
+`;
