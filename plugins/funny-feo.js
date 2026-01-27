2e7c5a35e440226e174430371689d7cf68bd102e
AdrianDH7 <adriandiazhurtado9@gmail.com>
2026-01-27 01:44:46 -0600

    Actualizar funny-feo.js

--- plugins/funny-feo.js
+++ plugins/funny-feo.js
@@ -3,12 +3,11 @@ export default {
   category: "funny",
 
   async run(sock, msg, args, ctx) {
-    const target =
-      msg.mentionedJid?.[0] ||
-      msg.quoted?.sender
+    const target = msg.mentionedJid?.[0] ||
+      msg.message?.extendedTextMessage?.contextInfo?.participant
 
     if (!target)
-      return sock.sendMessage(ctx.jid, { text: "🪞 Menciona o responde a alguien" })
+      return sock.sendMessage(ctx.jid, { text: "🪞 Menciona o responde a alguien" }, { quoted: msg })
 
     const name = target.split("@")[0]
 
