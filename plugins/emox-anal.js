ac894a7bcab75e888a99c79235cd5b87112d63ac
loversmessi62-cmyk <loversmessi62@gmail.com>
2026-03-27 15:50:05 -0600

    Actualizar emox-anal.js

--- plugins/emox-anal.js
+++ plugins/emox-anal.js
@@ -1,65 +1,62 @@
 //Codígo creado por Destroy wa.me/584120346669
 
-let handler = async (m, { conn, usedPrefix }) => {
+export default {
+  commands: ["anal", "culiar"],
+  category: "emox",
+  group: true,
+
+  async run(sock, msg, args, ctx) {
+    const jid = ctx.jid
+
     let who
+    const mentioned = msg.message?.extendedTextMessage?.contextInfo?.mentionedJid || []
 
-    const mentioned = m.mentionedJid || []
-
-    // Verificamos si se menciona a alguien o se cita un mensaje
+    // detectar mención o respuesta
     if (mentioned.length > 0) {
-        who = mentioned[0]
-    } else if (m.quoted) {
-        who = m.quoted.sender
+      who = mentioned[0]
+    } else if (msg.message?.extendedTextMessage?.contextInfo?.participant) {
+      who = msg.message.extendedTextMessage.contextInfo.participant
     } else {
-        who = m.sender
+      who = msg.key.participant || msg.key.remoteJid
     }
 
-    let name = await conn.getName(who)
-    let name2 = await conn.getName(m.sender)
+    const getName = async (id) => {
+      try {
+        return await sock.getName(id)
+      } catch {
+        return id.split("@")[0]
+      }
+    }
 
-    // evitar error si no existe react
-    if (m.react) m.react('🥵')
+    let name = await getName(who)
+    let name2 = await getName(msg.key.participant || msg.key.remoteJid)
 
     let str
     if (mentioned.length > 0) {
-        str = `\`${name2}\` le partio el culo a la puta de \`${name || who}\`.`
-    } else if (m.quoted) {
-        str = `\`${name2}\` se la metio en el ano a \`${name || who}\`.`
+      str = `\`${name2}\` le partio el culo a la puta de \`${name}\`.`
+    } else if (msg.message?.extendedTextMessage?.contextInfo?.participant) {
+      str = `\`${name2}\` se la metio en el ano a \`${name}\`.`
     } else {
-        str = `\`${name2}\` esta haciendo un anal`
+      str = `\`${name2}\` esta haciendo un anal`
     }
 
-    if (m.isGroup) {
-        let videos = [
-            'https://telegra.ph/file/7185b0be7a315706d086a.mp4',
-            'https://telegra.ph/file/a11625fef11d628d3c8df.mp4',
-            'https://telegra.ph/file/062b9506656e89b069618.mp4',
-            'https://telegra.ph/file/1325494a54adc9a87ec56.mp4',
-            'https://qu.ax/KKazS.mp4',
-            'https://qu.ax/ieJeB.mp4',
-            'https://qu.ax/MCdGn.mp4'
-        ]
+    const videos = [
+      'https://telegra.ph/file/7185b0be7a315706d086a.mp4',
+      'https://telegra.ph/file/a11625fef11d628d3c8df.mp4',
+      'https://telegra.ph/file/062b9506656e89b069618.mp4',
+      'https://telegra.ph/file/1325494a54adc9a87ec56.mp4',
+      'https://qu.ax/KKazS.mp4',
+      'https://qu.ax/ieJeB.mp4',
+      'https://qu.ax/MCdGn.mp4'
+    ]
 
-        const video = videos[Math.floor(Math.random() * videos.length)]
+    const video = videos[Math.floor(Math.random() * videos.length)]
 
-        let mentions = [who]
-
-        await conn.sendMessage(
-            m.chat,
-            {
-                video: { url: video },
-                gifPlayback: true,
-                caption: str,
-                mentions
-            },
-            { quoted: m }
-        )
-    }
+    await sock.sendMessage(jid, {
+      video: { url: video },
+      gifPlayback: true,
+      caption: str,
+      mentions: [who]
+    }, { quoted: msg })
+  }
 }
-
-handler.help = ['anal/culiar @tag']
-handler.tags = ['emox']
-handler.command = ['anal', 'culiar']
-handler.group = true
-
-export default handler
