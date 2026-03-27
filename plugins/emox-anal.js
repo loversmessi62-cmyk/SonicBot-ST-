f547fa0cefbec6af80db653336fc8ac3a19bb073
loversmessi62-cmyk <loversmessi62@gmail.com>
2026-03-27 15:45:38 -0600

    Crear emox-anal.js

---
+++ plugins/emox-anal.js
@@ -0,0 +1,65 @@
+//Codígo creado por Destroy wa.me/584120346669
+
+let handler = async (m, { conn, usedPrefix }) => {
+    let who
+
+    const mentioned = m.mentionedJid || []
+
+    // Verificamos si se menciona a alguien o se cita un mensaje
+    if (mentioned.length > 0) {
+        who = mentioned[0]
+    } else if (m.quoted) {
+        who = m.quoted.sender
+    } else {
+        who = m.sender
+    }
+
+    let name = await conn.getName(who)
+    let name2 = await conn.getName(m.sender)
+
+    // evitar error si no existe react
+    if (m.react) m.react('🥵')
+
+    let str
+    if (mentioned.length > 0) {
+        str = `\`${name2}\` le partio el culo a la puta de \`${name || who}\`.`
+    } else if (m.quoted) {
+        str = `\`${name2}\` se la metio en el ano a \`${name || who}\`.`
+    } else {
+        str = `\`${name2}\` esta haciendo un anal`
+    }
+
+    if (m.isGroup) {
+        let videos = [
+            'https://telegra.ph/file/7185b0be7a315706d086a.mp4',
+            'https://telegra.ph/file/a11625fef11d628d3c8df.mp4',
+            'https://telegra.ph/file/062b9506656e89b069618.mp4',
+            'https://telegra.ph/file/1325494a54adc9a87ec56.mp4',
+            'https://qu.ax/KKazS.mp4',
+            'https://qu.ax/ieJeB.mp4',
+            'https://qu.ax/MCdGn.mp4'
+        ]
+
+        const video = videos[Math.floor(Math.random() * videos.length)]
+
+        let mentions = [who]
+
+        await conn.sendMessage(
+            m.chat,
+            {
+                video: { url: video },
+                gifPlayback: true,
+                caption: str,
+                mentions
+            },
+            { quoted: m }
+        )
+    }
+}
+
+handler.help = ['anal/culiar @tag']
+handler.tags = ['emox']
+handler.command = ['anal', 'culiar']
+handler.group = true
+
+export default handler
