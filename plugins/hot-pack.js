a2b2193adfaea00c534136f0dc03d01cabe49b68
orlandopersonal339-spec <orlandopersonal339@gmail.com>
2026-05-14 14:12:05 -0600

    Actualizar hot-pack.js

--- plugins/hot-pack.js
+++ plugins/hot-pack.js
@@ -3,23 +3,21 @@ export default {
     category: "hot",
 
     async run(sock, msg, args, ctx) {
+
         const jid = ctx.jid;
 
+        // 🔥 LINKS DIRECTOS (ejemplo corregido)
         const links = [
-            "https://ibb.co/XkjVyd9W",
-            "https://ibb.co/c71YVLX",
-            "https://ibb.co/6R8YQRTM"
+            "https://i.ibb.co/XkjVyd9W.jpg",
+            "https://i.ibb.co/c71YVLX.jpg",
+            "https://i.ibb.co/6R8YQRTM.jpg"
         ];
 
-        if (!links.length)
-            return sock.sendMessage(jid, { text: "⚠ No hay links en .pack" });
-
         const random = links[Math.floor(Math.random() * links.length)];
-        const isVideo = random.endsWith(".mp4") || random.endsWith(".mov");
 
         await sock.sendMessage(jid, {
-            [isVideo ? "video" : "image"]: { url: random },
+            image: { url: random },
             caption: "📦 *PACK*"
-        });
+        }, { quoted: msg });
     }
 };
